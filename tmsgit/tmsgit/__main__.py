10b4aa953dfd3611a1347947f3f23aade37958c3
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-12-21 14:00:51 +0100

    Add commit command to cli.

--- tmsgit/tmsgit/__main__.py
+++ tmsgit/tmsgit/__main__.py
@@ -1,6 +1,14 @@
 import argparse
 
 
+def register_commit_parser(subparsers):
+    commit_parser = subparsers.add_parser('commit', help='Create a commit including current changes and [wip] previous commits')
+
+
+def register_save_parser(subparsers):
+    save_parser = subparsers.add_parser('save', help='Create a temporary [wip] commit that wont appear in final history')
+
+
 def register_create_parser(subparsers):
     create_parser = subparsers.add_parser('create', help='Create branch of given type')
     create_parser.add_argument('branch_type', choices=['feature', 'hotfix', 'bugfix', 'devops'])
@@ -11,6 +19,8 @@ def parse_args():
     subparsers = parser.add_subparsers(dest='command')
 
     register_create_parser(subparsers)
+    register_save_parser(subparsers)
+    register_commit_parser(subparsers)
 
     args = parser.parse_args()
     if not args.command:
@@ -26,3 +36,9 @@ def main():
     if args.command == 'create':
         from .commands import create_branch
         create_branch(args.branch_type)
+    elif args.command == 'save':
+        from .commands import save_current_changes
+        save_current_changes()
+    elif args.command == 'commit':
+        from .commands import create_commit
+        create_commit()
--- tmsgit/tmsgit/commands.py
+++ tmsgit/tmsgit/commands.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 def input_branch_name(branch_type):
     description = ""
     while len(description) < 4 or len(description) > 30:
@@ -7,8 +8,15 @@ def input_branch_name(branch_type):
         if len(description) < 4 or len(description) > 30:
             print('len(description) >= 4 and len(description) <= 30 ===> current:', len(description))
     return branch_type.lower() + '/' + description.lower().replace(' ', '_')
-    
 
+
+def input_commit_message():
+    message = ""
+    while len(message) < 10 or len(message) > 60:
+        message = input('commit message: ').lower()
+        if len(message) < 10 or len(message) > 60:
+            print('len(message) >= 4 and len(message) <= 30 ===> current:', len(message))
+    return message[0].upper() + message[1:] + "."*(not message.endswith('.'))
 
 
 def create_branch(branch_type):
@@ -22,4 +30,44 @@ def create_branch(branch_type):
     # create branch locally and push it to remote
     subprocess.call(['git', 'checkout', '-b', branch_name])
     subprocess.call(['git', 'push', '-u', 'origin', branch_name])
+
+
+def save_current_changes():
+    """
+    Generate a [wip] commit that won't be indexed in changelog
+    """
+    subprocess.call(['git', 'add', '.'])
+    subprocess.call(['git', 'commit', '-m', '[wip]'])
+
+
+def extract_commits_from_logs(command):
+    import re
+    LOG_COMMIT_PATTERN = re.compile("(?:^|\n)commit\s+(?P<commit_sha>\w+).+\nAuthor:\s+(?P<author>.+)\nDate:\s+(?P<date>.+)\n\n\s+(?P<message>.+)")
     
+    p = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output = p.stdout.read().decode('utf-8').strip()
+    return [m.groupdict() for m in LOG_COMMIT_PATTERN.finditer(output)]
+
+
+def get_saved_commits():
+    commits = []
+    for commit in extract_commits_from_logs(['git', 'log']):
+        if commit['message']!='[wip]':
+            break
+        commits.append(commit)
+    return commits
+
+
+def create_commit():
+    # save all changes in a wip commit
+    save_current_changes()
+
+    # get number of wip commits to squash
+    saved_commits = get_saved_commits()
+    
+    # number of commits to be squashed
+    message = input_commit_message()
+    squash_number = len(saved_commits)
+
+    subprocess.call(["git", "reset", "--soft", "HEAD~%s"%squash_number])
+    subprocess.call(['git', 'commit', '-m', message])
